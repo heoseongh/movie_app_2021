2d2fddeb72a590328dc4bbff1754e71827d3368a
heoseongh <shheo0807@gmail.com>
2021-01-03 17:33:15 +0900

    componentDidMount 사용

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React from "react";
 
 class App extends React.Component {
-
   state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState(current => ({ count: current.count + 1}));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count: current.count -1 }));
-  };
-
+    isLoading: true
+  }
+  
   componentDidMount() {
-    console.log("Component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false})
+    }, 6000);
   }
-
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-
+  
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>This number is : {this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
-    )
+    const { isLoading } = this.state;
+    return <div>{ isLoading ? "Loading..." : "We are ready" }</div>;
   }
-
 }
 
 export default App;
