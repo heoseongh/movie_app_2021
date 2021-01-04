b1b7c60b722b3abb7d348555526d1e4936acaae9
heoseongh <shheo0807@gmail.com>
2021-01-04 11:31:25 +0900

    비동기 방식

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import axios from "axios"
 
 class App extends React.Component {
   state = {
     isLoading: true
   }
   
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json")
+    console.log(movies);
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false})
-    }, 6000);
+    this.getMovies();
   }
   
   render() {
