df4518c8b6deee54d247895259a7d68a39413d6c
heoseongh <shheo0807@gmail.com>
2021-01-07 17:22:42 +0900

    css 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
+
 
 class App extends React.Component {
   state = {
@@ -9,7 +11,11 @@ class App extends React.Component {
   };
 
   getMovies = async () => {
-    const {data: {data: { movies } } } = await axios.get(
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
       "https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating"
     );
     this.setState({ movies, isLoading: false });
@@ -21,11 +27,16 @@ class App extends React.Component {
 
   render() {
     const { isLoading, movies } = this.state;
+
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map((movie) => (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
               <Movie
                 key={movie.id}
                 id={movie.id}
@@ -33,9 +44,12 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
-              /> 
+                genres={movie.genres}
+              />
             ))}
-      </div>
+          </div>
+        )}
+      </section>
     );
   }
 }
